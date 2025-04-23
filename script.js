9a57f9f8ca1378295a7d49fcd2c68925dd7bc93a
Ahmed <hamoudi1997@yahoo.com>
2025-04-22 21:27:49 -0400

    ProductProperties class and initial properties created

---
+++ script.js
@@ -0,0 +1,9 @@
+// Base Class
+
+class ProductProperties {
+  constructor(name, price, quantity) {
+    this.name = "Name of the product";
+    this.price = "Price of the product";
+    this.quantity = "Quantity available in stock";
+  }
+}
